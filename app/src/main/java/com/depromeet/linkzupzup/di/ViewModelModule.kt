a280ab5b169f9e7962877c5249a222da9cf1fc7f
mns0304 <djawl2918>
2021-04-24 20:22:17 +0900

    스크랩 상세화면 UI 개발 #3 - ScrapDetailActivity.kt Compose UI

--- app/src/main/java/com/depromeet/linkzupzup/di/ViewModelModule.kt
+++ app/src/main/java/com/depromeet/linkzupzup/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.depromeet.linkzupzup.di
 
 import com.depromeet.linkzupzup.presenter.MainViewModel
+import com.depromeet.linkzupzup.presenter.ScrapDetailViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MainViewModel(get()) }
+    viewModel { ScrapDetailViewModel(get()) }
 }
---
+++ app/src/main/java/com/depromeet/linkzupzup/view/scrap/ScrapDetailActivity.kt
@@ -0,0 +1,18 @@
+package com.depromeet.linkzupzup.view.scrap
+
+import android.os.Bundle
+import com.depromeet.linkzupzup.base.BaseActivity
+import com.depromeet.linkzupzup.presenter.ScrapDetailViewModel
+import com.depromeet.linkzupzup.view.scrap.ui.ScrapDetailUI
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+
+class ScrapDetailActivity : BaseActivity<ScrapDetailUI, ScrapDetailViewModel>() {
+
+    override var view: ScrapDetailUI = ScrapDetailUI()
+    override fun onCreateViewModel(): ScrapDetailViewModel = getViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
