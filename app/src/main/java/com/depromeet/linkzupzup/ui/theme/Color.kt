d6572dfe69633a97b791021eb87d551c65076376
Jackson <mns0304@gmail.com>
2021-04-27 02:23:21 +0900

    상단 StatusBar 배경 색상 및 글자 색상 변경 하단 NavigationBar 배경 색상 변경

--- app/src/main/java/com/depromeet/linkzupzup/ui/theme/Color.kt
+++ app/src/main/java/com/depromeet/linkzupzup/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.depromeet.linkzupzup.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Purple200 = Color(0xFFFFFFFF)
+val Purple500 = Color(0xFFDFDFDF)
+val Purple700 = Color(0xFFBFBFBF)
 val Teal200 = Color(0xFF03DAC5)
 
 val TagBgColor01 = Color(0xFFFFECEC)
