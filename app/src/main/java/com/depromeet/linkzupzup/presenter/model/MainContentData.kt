fdc264591bb3b92243cbd2b0dcda0399a2da01a2
Jackson <mns0304@gmail.com>
2021-05-10 08:57:04 +0900

    MainUI 코드정리 - TopAppBar 분리 ( AppBar IconButton 분리 ) - LazyColumn Type 분리 ( MAIN_TOP_HEADER, MAIN_LINK_ITEM ) - mock data 적용 - 일부 디자인 가이드에 맞게 수정 - 리스트 하단 영역 그라데이션 수정 ( drawWithCache )

---
+++ app/src/main/java/com/depromeet/linkzupzup/presenter/model/MainContentData.kt
@@ -0,0 +1,27 @@
+package com.depromeet.linkzupzup.presenter.model
+
+class MainContentData <T: Any> {
+
+    companion object {
+        const val EMPTY_TYPE = Int.MIN_VALUE
+        const val MAIN_TOP_HEADER = EMPTY_TYPE + 100
+        const val MAIN_LINK_ITEM = MAIN_TOP_HEADER + 100
+
+        fun mockMainContentList(cnt: Int): ArrayList<MainContentData<LinkData>> {
+            return arrayListOf<MainContentData<LinkData>>().apply {
+                repeat(cnt) {
+                    add(MainContentData(MAIN_LINK_ITEM, LinkData.mockData()))
+                }
+            }
+        }
+    }
+
+    var type: Int = EMPTY_TYPE
+    var data: T? = null
+
+    constructor()
+    constructor(type: Int, data: T? = null) {
+        this.type = type
+        this.data = data
+    }
+}
--- app/src/main/java/com/depromeet/linkzupzup/view/main/MainActivity.kt
+++ app/src/main/java/com/depromeet/linkzupzup/view/main/MainActivity.kt
@@ -2,13 +2,13 @@ package com.depromeet.linkzupzup.view.main
 
 import android.os.Bundle
 import com.depromeet.linkzupzup.base.BaseActivity
-import com.depromeet.linkzupzup.view.main.ui.MainUI
 import com.depromeet.linkzupzup.presenter.MainViewModel
+import com.depromeet.linkzupzup.view.main.ui.MainUI2
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class MainActivity : BaseActivity<MainUI, MainViewModel>() {
+class MainActivity : BaseActivity<MainUI2, MainViewModel>() {
 
-    override var view: MainUI = MainUI()
+    override var view: MainUI2 = MainUI2()
     override fun onCreateViewModel(): MainViewModel = getViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
