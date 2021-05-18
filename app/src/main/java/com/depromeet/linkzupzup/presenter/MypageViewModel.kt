c9e073b1a718129ece12f43fbf60f06a591bca18
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2021-05-18 12:21:28 +0900

    Mypage 추가 #11
    - activity,viewmodel,ui 추가

---
+++ app/src/main/java/com/depromeet/linkzupzup/presenter/MypageViewModel.kt
@@ -0,0 +1,6 @@
+package com.depromeet.linkzupzup.presenter
+
+import com.depromeet.linkzupzup.base.BaseViewModel
+
+class MypageViewModel : BaseViewModel() {
+}
---
+++ app/src/main/java/com/depromeet/linkzupzup/view/mypage/ui/MypageUI.kt
@@ -0,0 +1,12 @@
+package com.depromeet.linkzupzup.view.mypage.ui
+
+import androidx.compose.runtime.Composable
+import com.depromeet.linkzupzup.base.BaseView
+import com.depromeet.linkzupzup.presenter.MypageViewModel
+
+class MypageUI: BaseView<MypageViewModel>() {
+    @Composable
+    override fun onCreateViewContent() {
+
+    }
+}
