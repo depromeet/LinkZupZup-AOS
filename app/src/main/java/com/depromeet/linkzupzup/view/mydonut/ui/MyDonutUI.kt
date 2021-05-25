a1a74ace1d4d80b75b7956774d93e546f9b64799
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2021-05-25 17:42:26 +0900

    포인트, 도넛 기준 안내화면 추가중 (1) #23
    - 추가 화면 구성 중
    - LazyColumn Divider 추가 필요
    - 맨 아래 '포인트추가적립안내' bullet list로 변경 필요

--- app/src/main/java/com/depromeet/linkzupzup/view/mydonut/ui/MyDonutUI.kt
+++ app/src/main/java/com/depromeet/linkzupzup/view/mydonut/ui/MyDonutUI.kt
@@ -5,20 +5,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -36,6 +39,8 @@ import com.depromeet.linkzupzup.presenter.model.DonutBadge
 import com.depromeet.linkzupzup.presenter.model.MyDonutData
 import com.depromeet.linkzupzup.ui.theme.*
 import com.depromeet.linkzupzup.utils.DLog
+import com.depromeet.linkzupzup.view.custom.BottomSheetCloseBtn
+import kotlinx.coroutines.launch
 
 class MyDonutUI : BaseView<MyDonutViewModel>() {
 
@@ -66,7 +71,8 @@ fun MyDonutBodyUI(myDonutList: ArrayList<MyDonutData<*>>){
         backgroundColor = Color.Transparent,
         modifier = Modifier.fillMaxSize()) {
 
-        Column(modifier = Modifier.fillMaxWidth()
+        Column(modifier = Modifier
+            .fillMaxWidth()
             .background(Color.Transparent)) {
 
             Information()
@@ -80,11 +86,13 @@ fun MyDonutBodyUI(myDonutList: ArrayList<MyDonutData<*>>){
 @Composable
 fun MyDonutTopBar(){
     val ctx = LocalContext.current
-    Box(modifier = Modifier.fillMaxWidth()
+    Box(modifier = Modifier
+        .fillMaxWidth()
         .height(52.dp)){
 
         Row(verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.align(Alignment.CenterStart)
+            modifier = Modifier
+                .align(Alignment.CenterStart)
                 .fillMaxHeight()){
 
             MyDonutBackButton(painterResource(id = R.drawable.ic_detail_back)){
@@ -114,13 +122,15 @@ fun MyDonutBackButton(painter: Painter, onClick : () -> Unit){
     Card(elevation = 0.dp,
         shape = RoundedCornerShape(0),
         backgroundColor = Color.Transparent,
-        modifier = Modifier.fillMaxHeight()
+        modifier = Modifier
+            .fillMaxHeight()
             .wrapContentWidth()
             .noRippleClickable(onClick = onClick)) {
 
         Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
-            modifier = Modifier.size(56.dp)
+            modifier = Modifier
+                .size(56.dp)
                 .padding(start = 16.dp)) {
 
             Image(painter = painter,
@@ -133,18 +143,21 @@ fun MyDonutBackButton(painter: Painter, onClick : () -> Unit){
 
 @Composable
 fun Information(){
-    Row(modifier = Modifier.fillMaxWidth()
+    Row(modifier = Modifier
+        .fillMaxWidth()
         .padding(start = 16.dp, top = 12.dp)){
 
         Card(elevation = 0.dp,
             backgroundColor = Blue50Op8,
             shape = RoundedCornerShape(8.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .height(32.dp)){
 
             Row(verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .padding(horizontal = 16.dp)){
 
                 Image(painter = painterResource(id = R.drawable.ic_question),
@@ -168,19 +181,22 @@ fun Information(){
 @Preview
 @Composable
 fun NoDonut(){
-    Row(modifier = Modifier.fillMaxWidth()
+    Row(modifier = Modifier
+        .fillMaxWidth()
         .padding(top = 4.dp)){
 
         Card(elevation = 0.dp,
             backgroundColor = Blue50Op8,
             shape = RoundedCornerShape(8.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .height(64.dp)
                 .padding(horizontal = 6.dp)){
 
             Row(verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(vertical = 16.dp)){
 
                 Text(
@@ -217,8 +233,10 @@ fun DonutBadgeGridList(myDonutList: ArrayList<MyDonutData<*>>){
 
                 // 도넛이 있을 경우
                 MyDonutData.HAVE_DONUT -> (badge.data as? DonutBadge)?.let{ badgeData ->
-                    Box(Modifier.fillMaxSize()
-                        .padding(6.dp)) {
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(6.dp)) {
                         DonutBadgeCard(painterResource(id = badgeData.badgeResource),badgeData.point, badgeData.date)
                     }
                 }
@@ -237,14 +255,18 @@ fun DonutBadgeCard(painter : Painter, point : Int, date : String){
 
     Card(shape = RoundedCornerShape(0.dp),
         elevation = 0.dp,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .height(130.dp)) {
 
-        Column(Modifier.fillMaxSize()
-            .padding(top = 20.dp, bottom = 8.dp)) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp, bottom = 8.dp)) {
 
             // 뱃지
-            Row(modifier = Modifier.fillMaxWidth()
+            Row(modifier = Modifier
+                .fillMaxWidth()
                 .padding(horizontal = 20.dp)){
 
                 Image(painter = painter,
@@ -297,5 +319,248 @@ fun DonutPreview() {
             }
             MyDonutBodyUI(myDonutList)
         }
+    }
+}
+
+@Preview
+@Composable
+fun InfoBottomSheet(){
+
+    Surface(modifier = Modifier.background(Color.White)) {
+
+        InfoSheetHeader()
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(580.dp)
+                .padding(bottom = 16.dp)
+                .background(Color.Transparent)) {
+
+            // 닫기 버튼
+            Row(horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)) {
+
+                BottomSheetCloseBtn(painterResource(id = R.drawable.ic_close)){
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(64.dp))
+
+            // 메인 리스트
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .drawWithCache {
+                        val gradient = Brush.linearGradient(
+                            colors = listOf(Color.White, Color.Transparent),
+                            start = Offset(0f, 0f),
+                            end = Offset(0f, 64.dp.toPx())
+                        )
+                        onDrawWithContent {
+                            drawContent()
+                            drawRect(gradient)
+                        }
+                    }){
+                items(MyDonutData.donutInfo){ info->
+                    info?.let{
+                        InfoDonutCard(painter = painterResource(info.badgeResource), info = info.getInfoStr())
+                    } ?: run{
+                        AdditionalPointInfo()
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun InfoSheetHeader(){
+    // 로고가 닫기버튼 row 와 겹쳐있기 때문에 box
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 32.dp)
+        .background(Color.Transparent)){
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()){
+            Image(
+                painter = painterResource(id = R.drawable.ic_jubjub),
+                contentDescription = null,
+                modifier = Modifier.size(123.dp,49.dp))
+
+            val guideStr = AnnotatedString(
+                text = "링크 1개를 읽을 때마다 100 point가 쌓여요!",
+                spanStyles = listOf(
+                    AnnotatedString.Range(
+                        start = 0,
+                        end = 5,
+                        item = SpanStyle(
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(
+                                resId = R.font.spoqa_hansansneo_bold,
+                                weight = FontWeight.W700)))
+                    ),
+                    AnnotatedString.Range(
+                        start = 14,
+                        end = 23,
+                        item = SpanStyle(
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(
+                                resId = R.font.spoqa_hansansneo_bold,
+                                weight = FontWeight.W700)))
+                    )
+                )
+            )
+
+            Text(guideStr,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    lineHeight = 16.8.sp,
+                    color= Gray70,
+                    fontFamily = FontFamily(Font(
+                        resId = R.font.spoqa_hansansneo_regular,
+                        weight = FontWeight.W500))))
+            }
+    }
+}
+
+@Composable
+fun InfoDonutCard(painter : Painter = painterResource(id = R.drawable.ic_donut01), info : String){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .height(85.dp)
+        .background(Color.Transparent)){
+
+        Row(horizontalArrangement = Arrangement.spacedBy(25.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)){
+            Image(
+                modifier = Modifier.size(72.dp,52.dp),
+                painter = painter,
+                contentDescription = null)
+            
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "도넛 획득 조건",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        lineHeight = 16.8.sp,
+                        fontFamily = FontFamily(Font(
+                            resId = R.font.spoqa_hansansneo_bold,
+                            weight = FontWeight.W700))))
+
+                Text(
+                    text = info,
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        lineHeight = 16.8.sp,
+                        fontFamily = FontFamily(Font(
+                            resId = R.font.spoqa_hansansneo_regular,
+                            weight = FontWeight.W500))))
+            }
+        }
+    }
+
+}
+
+@Composable
+fun AdditionalPointInfo(){
+
+    val guideStr1 = AnnotatedString(
+        text = "5번째, 10번째, 15번째 등 읽은 아티클 수가 5의 배수에 해당되는 경우 20 point가 추가로 적립되며 읽은 아티클의 수는 하루마다 갱신됩니다.",
+        spanStyles = listOf(
+            AnnotatedString.Range(
+                start = 29,
+                end = 34,
+                item = SpanStyle(color = Color(0xff1f1f1f),
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily(Font(
+                        resId = R.font.spoqa_hansansneo_regular,
+                        weight = FontWeight.W500)))
+            ),
+            AnnotatedString.Range(
+                start = 44,
+                end = 52,
+                item = SpanStyle(color = Color(0xff1f1f1f),
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily(Font(
+                        resId = R.font.spoqa_hansansneo_regular,
+                        weight = FontWeight.W500)))
+            )
+        )
+    )
+
+    val guideStr2 = AnnotatedString(
+        text = "7일 연속으로 접속하여 아티클을 읽은 경우 700 point가 추가로 지급되며 지급 후 연속 일수는 초기화됩니다.",
+        spanStyles = listOf(
+            AnnotatedString.Range(
+                start = 0,
+                end = 5,
+                item = SpanStyle(color = Color(0xff1f1f1f),
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily(Font(
+                        resId = R.font.spoqa_hansansneo_bold,
+                        weight = FontWeight.W700)))
+            ),
+            AnnotatedString.Range(
+                start = 24,
+                end = 33,
+                item = SpanStyle(color = Color(0xff1f1f1f),
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily(Font(
+                        resId = R.font.spoqa_hansansneo_bold,
+                        weight = FontWeight.W700)))
+            )
+        )
+    )
+
+
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xfff5f5f5))) {
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "포인트 추가 적립 안내",
+            style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+                fontFamily = FontFamily(Font(
+                    resId = R.font.spoqa_hansansneo_regular,
+                    weight = FontWeight.W500))))
+
+        Divider(color = Gray50t,
+            thickness = 1.dp,
+            modifier = Modifier
+                .padding(horizontal = 20.dp,vertical = 12.dp))
+
+        Text(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            text = guideStr1,
+            style = TextStyle(color = Color(0xff1f1f1f),
+                fontSize = 12.sp,
+                lineHeight = 16.8.sp,
+                fontFamily = FontFamily(Font(
+                    resId = R.font.spoqa_hansansneo_light,
+                    weight = FontWeight.W300))))
+
+        Text(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            text = guideStr2,
+            style = TextStyle(color = Color(0xff1f1f1f),
+                fontSize = 12.sp,
+                lineHeight = 16.8.sp,
+                fontFamily = FontFamily(Font(
+                    resId = R.font.spoqa_hansansneo_light,
+                    weight = FontWeight.W300))))
     }
 }
