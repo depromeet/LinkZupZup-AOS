a3e4888101eefe351dfe754920f382d74b0de3fa
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2021-05-31 15:41:01 +0900

    OnBoarding과 Main 액티비티 연결
    - Manifest에 Main 빠져있어서 추가
    - OnBoarding 브런치에서 이어서 개발하기 위해 Main과 미리 연결해둠.

--- app/src/main/java/com/depromeet/linkzupzup/view/onBoarding/OnBoardingActivity.kt
+++ app/src/main/java/com/depromeet/linkzupzup/view/onBoarding/OnBoardingActivity.kt
@@ -1,10 +1,14 @@
 package com.depromeet.linkzupzup.view.onBoarding
 
+import android.content.Intent
 import android.os.Bundle
 import com.depromeet.linkzupzup.architecture.presenterLayer.OnBoardingViewModel
 import com.depromeet.linkzupzup.base.BaseActivity
+import com.depromeet.linkzupzup.view.main.MainActivity
 import com.depromeet.linkzupzup.view.onBoarding.ui.OnBoardingUI
+import io.reactivex.Observable
 import org.koin.androidx.viewmodel.ext.android.getViewModel
+import java.util.concurrent.TimeUnit
 
 class OnBoardingActivity : BaseActivity<OnBoardingUI, OnBoardingViewModel>()  {
 
@@ -13,10 +17,19 @@ class OnBoardingActivity : BaseActivity<OnBoardingUI, OnBoardingViewModel>()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Observable.timer(2, TimeUnit.SECONDS)
+            .subscribe(this::nextPage)
     }
 
     override fun onBackPressed() {
         // super.onBackPressed()
+    }
+
+    private fun nextPage(time: Long) {
+        Intent(this, MainActivity::class.java)
+            .let(this::startActivity)
+        finish()
     }
 
 }
