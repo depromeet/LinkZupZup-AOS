03ed4b876fa2f41075c859b3c765f37fb6cab0fd
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2021-05-31 16:08:14 +0900

    OnBoarding (1) 배경이미지 추가 #33
    - Scale = Crop
    - Alignment = TopStart

--- app/src/main/java/com/depromeet/linkzupzup/view/onBoarding/ui/OnBoardingUI.kt
+++ app/src/main/java/com/depromeet/linkzupzup/view/onBoarding/ui/OnBoardingUI.kt
@@ -1,8 +1,17 @@
 package com.depromeet.linkzupzup.view.onBoarding.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.depromeet.linkzupzup.R
 import com.depromeet.linkzupzup.architecture.presenterLayer.OnBoardingViewModel
 import com.depromeet.linkzupzup.base.BaseView
 import com.depromeet.linkzupzup.ui.theme.Gray10
@@ -13,9 +22,36 @@ class OnBoardingUI  : BaseView<OnBoardingViewModel>() {
     @Composable
     override fun onCreateViewContent() {
         LinkZupZupTheme {
-            Surface(color = Gray10) {
-                Text(text = "onBoard")
+            Surface {
+                Box(modifier = Modifier.fillMaxSize()){
+                    OnBoardingBody()
+                }
+
             }
+        }
+    }
+}
+
+@Composable
+fun OnBoardingBody(){
+    Image(
+        painter = painterResource(id = R.drawable.ic_onboarding_bg),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+        alignment = Alignment.TopStart)
+}
+
+
+@Preview
+@Composable
+fun OnBoardingPreview(){
+    LinkZupZupTheme {
+        Surface {
+            Box(modifier = Modifier.fillMaxSize()){
+                OnBoardingBody()
+            }
+
         }
     }
 }
