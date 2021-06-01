eca6bdde56b1ce5bab0b7fb6d7026f6db021e002
skrud9704 <skrud9704@gmail.com>
2021-06-02 02:51:49 +0900

    메인리스트 (6) MainViewModel 코드 정리 #37
    - MainViewModel 기존 코드 노션으로 옮김
    - 사용하지 않는 메서드 삭제

--- app/src/main/java/com/depromeet/linkzupzup/architecture/presenterLayer/MainViewModel.kt
+++ app/src/main/java/com/depromeet/linkzupzup/architecture/presenterLayer/MainViewModel.kt
@@ -48,18 +48,7 @@ class MainViewModel(private val linkUseCases: LinkUseCases): BaseViewModel() {
     }
 
 
-
-
-
-
     val linkAlarmResponse: MutableLiveData<ResponseEntity<LinkAlarmDataEntity>> = MutableLiveData()
-
-    val _linkListLiveData: MutableLiveData<ArrayList<MainContentData<LinkData>>> = MutableLiveData()
-    val linkListLiveData: LiveData<ArrayList<MainContentData<LinkData>>> get() = _linkListLiveData
-
-    private val _linkList: ArrayList<MainContentData<LinkData>> = arrayListOf()
-    private val _urlList: ArrayList<String> = arrayListOf()
-    private val _metaList: MutableLiveData<ArrayList<LinkMetaInfoEntity>> = MutableLiveData(arrayListOf())
 
     /**
      * 사용자가 저장한 링크 리스트 조회
@@ -80,32 +69,7 @@ class MainViewModel(private val linkUseCases: LinkUseCases): BaseViewModel() {
                 .subscribe({ response ->
                     // linkAlarmResponse.value = response
 
-                    val coroutineScope : CoroutineScope = CoroutineScope(Dispatchers.IO)
-//                    response.apply {
-//                        data?.content?.map { it.linkURL }?.let { urlList ->
-//                            coroutineScope.launch {
-//                                // 의심 구간 1. list -> arrayList
-//                                linkUseCases.getMetaList(ArrayList(urlList)).let { metaList ->
-//                                    data = data?.content?.map {
-//                                        val target = metaList.find { m -> m.url == it.linkURL }
-//
-//                                        it.apply {
-//                                            target?.let { t ->
-//                                                metaTitle = t.title
-//                                                metaDescription = t.content
-//                                                metaImageUrl = t.imgUrl
-//                                            }
-//                                        }
-//                                    }?.let {
-//                                        linkAlarmResponse.value =
-//                                    }
-//                                }
-//
-//                            }
-//                        }
-//                    }
-
-
+                    val coroutineScope = CoroutineScope(Dispatchers.IO)
                     linkAlarmResponse.value = response.apply {
                         data = data?.apply {
                             // url 만 발췌
@@ -133,54 +97,6 @@ class MainViewModel(private val linkUseCases: LinkUseCases): BaseViewModel() {
                         }
                     }
 
-
-
-
-                    // 1. 링크 API 정보 저장, url 저장
-//                    it.data?.content?.forEach { linkAlarmEntity ->
-//                        _linkList.add(MainContentData(MainContentData.MAIN_LINK_ITEM,LinkData(linkAlarmEntity)))
-//                        _urlList.add(linkAlarmEntity.linkURL)
-//
-//                        DLog.e("TEST","add ${linkAlarmEntity.linkURL}")
-//                    }
-//
-//                    // 2. metaInfo 확인 - runBlocking
-//                    runBlocking {
-//                        launch(Dispatchers.IO) {
-//                            _metaList.value?.addAll(linkUseCases.getMetaList(_urlList))
-//                            DLog.e("TEST","2.5 _metalist.value size = ${_metaList.value?.size}")
-//                        }
-//                    }
-//
-//
-//                    DLog.e("TEST","3")
-//
-//                    // 3. 링크와 metaInfo 매핑
-//                    _linkList.apply {
-//                        this.map { link->
-//                            _metaList.value?.let{ meta->
-//                                meta.find { m-> m.url == link.data?.linkURL} ?.let{ find->
-//                                    link.data?.setMetaInfo(find)
-//                                    DLog.e("TEST","찾았다 ${find.url}")
-//                                }?: run {
-//                                    CoroutineScope(Dispatchers.IO).launch{
-//                                        link.data?.linkURL?.let { url ->
-//                                            DLog.e("TEST","링크 $url")
-//                                            extractUrlFormText(url)?.let{ rightUrl ->
-//                                                linkUseCases.insertMetaInfo(getMetaDataFromUrl(rightUrl))
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                    // 4. 매핑된 링크만 UI에 먼저 반영
-//                    _linkListLiveData.value?.addAll(_linkList)
-//                    _linkListLiveData.value = _linkListLiveData.value
-
-
                     progressStatus(false)
                 }, this@MainViewModel::defaultThrowable))
         }
@@ -198,10 +114,4 @@ class MainViewModel(private val linkUseCases: LinkUseCases): BaseViewModel() {
         }
     }
 
-    fun insertLink(link : LinkData){
-        // viewModel 에서 제공하는 coroutine scope
-        // viewModelScope.launch {
-        //     linkUseCases.insertLinkInfo(link = link)
-        // }
-    }
 }
