3e40cd69428478a5c6ce0c61127e2c03a0a6703a
mns0304 <djawl2918>
2021-06-19 15:01:56 +0900

    anko layout 확장

---
+++ app/src/main/java/com/depromeet/linkzupzup/extensions/ankoExtensions.kt
@@ -0,0 +1,6 @@
+package com.depromeet.linkzupzup.extensions
+
+//inline fun ViewManager.squareImageView() = squareImageView {}
+//inline fun ViewManager.squareImageView(init: SquareImageView.() -> Unit): SquareImageView {
+//    return ankoView({ SquareImageView(it) }, theme= 0, init= init)
+//}
